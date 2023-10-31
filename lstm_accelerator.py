ef0b9b98b2317a2fd70c66747017e7e7aa005cdc
Benjamin Chauhan <benjamin.chauhan@duke.edu>
2023-10-31 10:27:01 -0400

    format

--- lstm_accelerator.py
+++ lstm_accelerator.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 from torch.autograd import Variable
 
+
 class LSTM(nn.Module):
     def __init__(self, num_classes, input_size, hidden_size, num_layers, seq_length):
         super(LSTM, self).__init__()
@@ -16,19 +17,19 @@ class LSTM(nn.Module):
                             num_layers=num_layers, batch_first=True)
 
         self.fc = nn.Linear(hidden_size, num_classes)
-    
+
     def forward(self, x):
         h_0 = Variable(torch.zeros(
             self.num_layers, x.size(0), self.hidden_size))
-        
+
         c_0 = Variable(torch.zeros(
             self.num_layers, x.size(0), self.hidden_size))
-        
+
         # Propagate input through LSTM
         ula, (h_out, _) = self.lstm(x, (h_0, c_0))
-        
+
         h_out = h_out.view(-1, self.hidden_size)
-        
+
         out = self.fc(h_out)
-        
+
         return out
